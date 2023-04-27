2f39b57bab7c1b65c2aa34c7ff9ec62e49669da7
DisturbedMindSet <kl4usmik4elsonn@gmail.com>
2023-04-27 22:15:37 +0100

    Update Transaction.cs

--- MiniBudgetApp/Models/Transaction.cs
+++ MiniBudgetApp/Models/Transaction.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Drawing;
 
 namespace MiniBudgetApp.Models
 {
@@ -7,15 +8,78 @@ namespace MiniBudgetApp.Models
     {
         [Key]
         public Guid TransactionId { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Amount should be greater than 0.")]
         public int Amount { get; set; }
 
         [Column(TypeName = "nvarchar(75)")]
         public string? Note { get; set; }
         public DateTime Date { get; set; } = DateTime.Now;
 
-        
+
+        //[Range(1, int.MaxValue, ErrorMessage = "Please select a category.")]
+
+        [Required(ErrorMessage ="teste")]
+        [RequiredGuid(ErrorMessage = "Please select a category.")]
         public Guid CategoryId { get; set; }
         public Category? Category { get; set; }
+
+        [NotMapped] 
+        public string? CategoryTitleWithIcon 
+        { 
+            get
+            { 
+                return Category==null ? "" : Category.Icon + " " + Category.Name;
+            } 
+        }
+        [NotMapped]
+        public string? FormattedAmount
+        {
+            get
+            {
+                return ((Category == null || Category.Type == "Expense") ? "- " : "+ ") + Amount.ToString("C2");
+            }
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    public class NonEmptyGuidAttribute : ValidationAttribute
+    {
+
+        public override bool IsValid(Object value)
+        {
+            bool result = true;
+
+            if ((Guid)value == Guid.Empty)
+                result = false;
+
+            return result;
+        }
+
      
     }
+
+    public class RequiredGuidAttribute : RequiredAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            var guid = CastToGuidOrDefault(value);
+
+            return !Equals(guid, default(Guid));
+        }
+
+        private static Guid CastToGuidOrDefault(object value)
+        {
+            try
+            {
+                return (Guid)value;
+            }
+            catch (Exception e)
+            {
+                if (e is InvalidCastException || e is NullReferenceException) return default(Guid);
+                throw;
+            }
+        }
+    }
+
 }
