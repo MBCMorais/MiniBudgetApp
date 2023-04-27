d546217dafc6578706be4924dd8143b373701a0e
DisturbedMindSet <kl4usmik4elsonn@gmail.com>
2023-04-27 14:41:15 +0100

    Update Category.cs

--- MiniBudgetApp/Models/Category.cs
+++ MiniBudgetApp/Models/Category.cs
@@ -16,5 +16,14 @@ namespace MiniBudgetApp.Models
 
         [Column(TypeName = "nvarchar(10)")]
         public string Type { get; set; } = "Expense";
+
+        [NotMapped] 
+        public string TitlewithIcon {
+            get
+            {
+                return this.Icon + " " + this.Name;
+            } 
+        }
+
     }
 }
